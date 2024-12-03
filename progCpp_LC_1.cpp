d60e5b173591249d05027a89eb7d1716c184d3bb
leidiana-carvalho66 <valentinalourenco2014@gmail.com>
2024-12-03 09:52:48 +0000

    primeiro

---
+++ progCpp_LC_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    float height;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    cout << "Enter your height (in meters): ";
+    cin >> height;
+
+    cout << "Your age is: " << age << " years and your height is: " << height << " meters." << endl;
+
+    return 0;
+}
